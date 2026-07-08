c5989d483af2fd03579ec9d81c78c48cbda298eb
Putureanu Radu-Mihai <2radupmihai3@gmail.com>
2026-07-08 17:13:53 +0300

    Time: 59 ms (6.03%) | Memory: 19.6 MB (12.42%) - LeetSync

---
+++ 749-shortest-completing-word/shortest-completing-word.py
@@ -0,0 +1,17 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        f = collections.Counter()
+
+        for c in licensePlate.lower():
+            if c.isalpha():
+                f[c] += 1
+
+        shortest = "z" * 20
+
+        for word in words:
+            wf = collections.Counter(word)
+
+            if (wf & f) == f:
+                if len(word) < len(shortest):
+                    shortest = word
+        return shortest
